23ad567f28f88802f753739d373db2d1602cf203
khoazcho2 <vot835848@gmail.com>
2026-03-06 15:57:21 +0700

    Update server.py

--- server.py
+++ server.py
@@ -295,5 +295,9 @@ def verify_key():
 @app.route("/")
 def home():
     return "Server Flask đang chạy"
+
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
